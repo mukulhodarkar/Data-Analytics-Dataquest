49b8d75f228c6019ef8616fbd56307c69c8eccc4
Mukul <mukulhodarkar@users.noreply.github.com>
2017-12-19 11:26:35 -0500

    first commint from Atom editor

--- 3_Functions/1_cleaning_data.py
+++
@@ -1,8 +0,0 @@
-f = open('movie_metadata.csv', 'r')
-data = f.read()
-data = data.split('\n')
-movie_data = []
-for line in data:
-    record = line.split(',')
-    movie_data.append(record)
-print (movie_data)
--- part_2/classes/DefineDataset.py
+++
@@ -1,7 +0,0 @@
-class Dataset:
-    def __init__(self):
-        self.type = "csv"
-
-#Do not declare like this: dataset = new Dataset
-dataset = Dataset()
-print(dataset.type)
