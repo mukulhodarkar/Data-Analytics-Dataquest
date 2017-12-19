099998dc6cf0348d00eac07e37ff6725f96705fd
Mukul <mukulhodarkar@users.noreply.github.com>
2017-12-19 14:01:24 -0500

    Covert Birth year to int in the same loop

---
+++ part_2/Error Handling/ConvertBirthYearstoIntegers.py
@@ -0,0 +1,13 @@
+import csv
+
+file_data = open("legislators.csv", "r")
+legislators = list(csv.reader(file_data))
+for row in legislators:
+    birthday = row[2]
+    birth_year = birthday.split("-")[0]
+    try:
+        birth_year = int(birth_year)
+    except Exception:
+        birth_year = 0
+    row.append(birth_year)
+print(row)
