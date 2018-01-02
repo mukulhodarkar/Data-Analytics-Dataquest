43a3339a3e3dfbc6e12e16917d34006f3bf313b9
Mukul <mukulhodarkar@users.noreply.github.com>
2018-01-02 18:26:49 -0500

    List comprehensions are much more compact notation, and can save space when you need to write multiple for loops.

---
+++ part_2/list comprehensions/ListComprehensions.py
@@ -0,0 +1,7 @@
+apple_prices = [100, 101, 102, 105]
+
+apple_prices_doubled = [(price*2) for price in apple_prices]
+print(apple_prices_doubled)
+
+apple_prices_lowered = [(price-100) for price in apple_prices]
+print("reduced apple prices:",apple_prices_lowered)
